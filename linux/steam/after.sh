e2336dc6f8040f3a4a2ccae084fc82c265d46969
Travis Stephens <36552255+tsteph11@users.noreply.github.com>
2024-06-03 09:46:14 -0400

    Create after.sh

---
+++ linux/steam/after.sh
@@ -0,0 +1,4 @@
+#This is not a script just a series of commands to run after steam is installed to get other games such as megamanx legacy colleciton to run
+sudo dpkg --add-architecture i386
+sudo apt update
+sudo apt install libnvidia-gl-535:i386
