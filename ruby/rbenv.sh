2ca91f0a29c5638aec2a209d1498e07a616de9ec
Travis Stephens <36552255+tsteph11@users.noreply.github.com>
2024-07-22 11:24:28 -0400

    Update rbenv.sh

--- ruby/rbenv.sh
+++ ruby/rbenv.sh
@@ -1,5 +1,5 @@
 sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
-curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash -
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash 
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
