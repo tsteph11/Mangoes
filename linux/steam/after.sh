ec9cfdc6d33b9e4e3eabce790809d0f68a499577
Travis Stephens <36552255+tsteph11@users.noreply.github.com>
2024-07-22 11:22:19 -0400

    Update after.sh

--- linux/steam/after.sh
+++ linux/steam/after.sh
@@ -1,4 +1,5 @@
 #This is not a script just a series of commands to run after steam is installed to get other games such as megamanx legacy colleciton to run
 sudo dpkg --add-architecture i386
 sudo apt update
+#replace the 535 with the major version you get from running nvidia-settings
 sudo apt install libnvidia-gl-535:i386
